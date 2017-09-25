dc90fdd7a1a60ddfe37af9f53cc63ff98f9d1947
sandy1313133 <sandy1313love@gmail.com>
2017-09-25 14:43:20 +0800

    hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("106024026李宜珊");
+        System.out.print("資訊傳播學系");
+
+    }
+}
